6d00029257b78daee58c4a510492f7cfac18fe36
Rodrigo Nicola <rodrigo.nicola0@gmail.com>
2025-01-30 14:19:44 -0300

    Change docs section name

--- webpage/docusaurus.config.ts
+++ webpage/docusaurus.config.ts
@@ -57,7 +57,7 @@ const config: Config = {
     // Replace with your project's social card
     image: 'img/docusaurus-social-card.jpg',
     navbar: {
-      title: 'Template docusaurus',
+      title: 'Arquitetura de computadores',
       logo: {
         alt: 'Logo Firelink Lib.',
         src: 'img/firelink.png',
